72bc560f7543280804054fce5ea410d79e29209c
Tomoya Nagasawa <ocrybit@gmail.com>
2025-06-02 00:16:27 +0400

    [wao] process methods & cache_wasm_image helper

--- src/dev_wao.erl
+++ src/dev_wao.erl
@@ -1,5 +1,5 @@
 -module(dev_wao).
--export([info/3, relay/3]).
+-export([ info/3, relay/3, compute/3, init/3, snapshot/3, normalize/3, cache_wasm_image/3 ]).
 -include_lib("eunit/include/eunit.hrl").
 -include("include/hb.hrl").
 
@@ -7,29 +7,58 @@ info(Msg, _, Opts) ->
     {ok, hb_ao:set(Msg, #{ <<"version">> => <<"1.0">> }, Opts)}.
 
 relay(_Msg1, Msg2, Opts) ->
-    % Extract the forwarding parameters
+						% Extract the forwarding parameters
     Target = hb_ao:get(<<"forward-to">>, Msg2, undefined, Opts),
     Method = hb_ao:get(<<"forward-method">>, Msg2, <<"POST">>, Opts),
     Body = hb_ao:get(<<"forward-body">>, Msg2, <<>>, Opts),
-    
+
     case Target of
         undefined ->
             {error, <<"Missing forward-to header">>};
         _ ->
-            % Construct the relay message
+						% Construct the relay message
             RelayMsg = #{
-                <<"path">> => <<"/~relay@1.0/call">>,
-                <<"method">> => <<"POST">>,
-                <<"relay-path">> => Target,
-                <<"relay-method">> => Method,
-                <<"relay-body">> => Body
-            },
-            
-            % Forward to the relay device
+			 <<"path">> => <<"/~relay@1.0/call">>,
+			 <<"method">> => <<"POST">>,
+			 <<"relay-path">> => Target,
+			 <<"relay-method">> => Method,
+			 <<"relay-body">> => Body
+			},
+
+						% Forward to the relay device
             case hb_ao:resolve(RelayMsg, Opts) of
                 {ok, Response} ->
                     {ok, Response};
                 {error, Reason} ->
                     {error, Reason}
             end
+    end.
+
+compute(Msg1, Msg2, Opts) ->
+    case hb_ao:get([<<"body">>,<<"Action">>], Msg2, Opts) of
+	Other ->
+	    {ok, hb_ao:set( Msg1, #{  }, Opts )}
+    end.
+
+init(Msg, Msg2, Opts) -> 
+    {ok, hb_ao:set(Msg, #{ }, Opts)}.
+
+
+snapshot(Msg, _Msg2, _Opts) -> {ok, Msg}.
+
+normalize(Msg, _Msg2, _Opts) -> {ok, Msg}.
+
+cache_wasm_image(Msg1, _Msg2, Opts) ->
+    Filename = hb_ao:get(<<"filename">>, Msg1, <<>>, Opts),
+    case dev_wasm:cache_wasm_image(Filename, Opts) of
+        #{ <<"image">> := ImageID } ->
+            {ok, #{ <<"image">> => ImageID }};
+        Error ->
+            {ok, #{
+		   <<"content-type">> => <<"application/json">>,
+		   <<"body">> => dev_codec_json:to(#{
+						     <<"error">> => <<"Failed to cache WASM image">>,
+						     <<"details">> => iolist_to_binary(io_lib:format("~p", [Error]))
+						    })
+		  }}
     end.
